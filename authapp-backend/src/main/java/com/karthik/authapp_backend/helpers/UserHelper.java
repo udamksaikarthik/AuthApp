65c29ec77accbd712bdef70d83bb76cf85170576
udamksaikarthik <50781377+udamksaikarthik@users.noreply.github.com>
2026-02-13 13:33:37 +0000

    All Apis added to controller

---
+++ authapp-backend/src/main/java/com/karthik/authapp_backend/helpers/UserHelper.java
@@ -0,0 +1,9 @@
+package com.karthik.authapp_backend.helpers;
+
+import java.util.UUID;
+
+public class UserHelper {
+	public static UUID parseUUID(String uuid) {
+		return UUID.fromString(uuid);
+	}
+}
